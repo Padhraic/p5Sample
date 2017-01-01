012aa4cb6a576e87890a64503348a604500752c2
Padhraic <pcoogan@gmail.com>
2017-01-01 18:20:33 +0900

    color mandelbrot

--- mandelbrot/sketch.js
+++ mandelbrot/sketch.js
@@ -2,6 +2,40 @@
 var minSlider;
 var maxSlider;
 
+var rainbow = [{
+  r: 0,
+  g: 0,
+  b: 0
+}, {
+  r: 148,
+  g: 0,
+  b: 211
+}, {
+  r: 75,
+  g: 0,
+  b: 130
+}, {
+  r: 0,
+  g: 0,
+  b: 255
+}, {
+  r: 0,
+  g: 255,
+  b: 0
+}, {
+  r: 255,
+  g: 255,
+  b: 0
+}, {
+  r: 255,
+  g: 127,
+  b: 0
+}, {
+  r: 255,
+  g: 0,
+  b: 0
+}];
+
 function setup() {
   createCanvas(200, 200);
   pixelDensity(1);
@@ -32,16 +66,16 @@ function draw() {
         n++;
       }
 
-      var bright = map(n, 0, maxIterations, 0, 1);
-      bright = map(sqrt(bright), 0, 1, 0, 255);
-      if (n === maxIterations) {
-        bright = 0;
-      }
+      var bright = floor(map(n, 0, maxIterations, 7, 0));
+      //bright = map(sqrt(bright), 0, 1, 0, 255);
+      //if (n === maxIterations) {
+      //  bright = 0;
+      //}
 
       var pix = (x + y * width) * 4;
-      pixels[pix + 0] = bright;
-      pixels[pix + 1] = bright;
-      pixels[pix + 2] = bright;
+      pixels[pix + 0] = rainbow[bright].r;
+      pixels[pix + 1] = rainbow[bright].g;
+      pixels[pix + 2] = rainbow[bright].b;
       pixels[pix + 3] = 255;
     }
   }
